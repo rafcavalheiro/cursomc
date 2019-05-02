ada596f34dab880242c6d300dfecd4bf82dc7135
Rafael Cavalheiro <rafcavalheiro@gmail.com>
2019-05-02 15:44:20 -0300

    Pedido/ Estado e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -38,6 +38,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente () {
 		
 	}
@@ -86,6 +89,14 @@ public class Cliente implements Serializable {
 	public TipoCliente getTipo() {
 		return TipoCliente.toEnum(tipo);
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -131,6 +142,8 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+
+	
 	
 
 }
