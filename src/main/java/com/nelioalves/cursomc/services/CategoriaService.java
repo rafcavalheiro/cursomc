3006a714cd91c8eb02309384d1a8b9994af6030b
Rafael Cavalheiro <rafcavalheiro@gmail.com>
2019-05-03 16:57:40 -0300

    Listando todas as categorias em DTO

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,7 +51,11 @@ public class CategoriaService {
 		}
 		
 	}
-}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+		}
+	}
 	
 
 
